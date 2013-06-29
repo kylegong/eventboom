1e45195f71a944fbb32b733eec6d004275da0e7a
John Bjerke <bjerkeweb@gmail.com>
2013-06-29 16:54:44 -0700

    styles, updated views html

--- client/app/scripts/mock_data.js
+++ client/app/scripts/mock_data.js
@@ -4,7 +4,7 @@ window.Boom.Data = [
   {
     id: 1,
     title: 'My event',
-    description: 'This is awesome',
+    description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     date: new Date(2013, 6, 29),
     tags: ['sports']
   },
--- client/app/scripts/views/event_short.js
+++ client/app/scripts/views/event_short.js
@@ -2,6 +2,7 @@ window.Boom = window.Boom || {}
 
 window.Boom.EventShortView = Backbone.View.extend({
   className: 'event',
+  tagName: 'li',
   template: _.template($('#event-short').html()),
   render: function() {
     this.$el.html(
