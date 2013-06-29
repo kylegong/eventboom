68f403ac11ece8f694743c69f3d1e51744fac6ef
Karl Mendes <karl@darbysmart.com>
2013-06-29 16:42:30 -0700

    Filter by date

--- client/app/scripts/mock_data.js
+++ client/app/scripts/mock_data.js
@@ -5,21 +5,21 @@ window.Boom.Data = [
     id: 1,
     title: 'My event',
     description: 'This is awesome',
-    date: new Date(2013, 6, 29),
+    date: new Date(2013, 5, 30),
     tags: ['sports']
   },
   {
     id: 2,
     title: 'Other event',
     description: 'No way!!!',
-    date: new Date(2013, 6, 28),
+    date: new Date(2013, 5, 29),
     tags: ['food', 'fun']
   },
   {
     id: 3,
     title: 'I\'m not going',
     description: 'No way!!!',
-    date: new Date(2013, 7, 3),
+    date: new Date(2013, 6, 3),
     tags: ['speed dating']
   }
 ]
--- client/app/scripts/models/event.js
+++ client/app/scripts/models/event.js
@@ -5,5 +5,25 @@ window.Boom.EventModel = Backbone.Model.extend({});
 window.Boom.EventCollection = Backbone.Collection.extend({
   comparator: function(event) {
     return event.get('date');
+  },
+  sortAndFilter: function(when) {
+    var filtered,
+        self = this;
+    this.data = this.data || this.toJSON();
+    if(when) {
+      when = parseInt(when, 10);
+      filtered = _.filter(this.data, function(event) {
+        return self.isInTheNextXDays(when, event.date);
+      });
+      this.reset(filtered);
+    } else {
+      this.reset(this.data);
+    }
+  },
+  isInTheNextXDays: function(days, date) {
+    var today = new Date(),
+        limit = new Date();
+    limit.setDate(today.getDate() + days);
+    return date <= limit;
   }
 });
