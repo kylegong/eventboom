938f8ae6a94695a0fcfd113dfda10671016e6858
George Truong <truong.george@gmail.com>
2013-06-29 21:03:32 -0700

    Add basic join event flow

--- client/app/scripts/views/event.js
+++ client/app/scripts/views/event.js
@@ -4,10 +4,32 @@ window.Boom.EventView = Backbone.View.extend({
   el: '#main',
   className: 'event',
   template: _.template($('#event').html()),
+  events: {
+    'click .show-form':   'showForm',
+    'click .join-event':  'joinEvent'
+  },
   render: function() {
     this.$el.html(
       this.template(this.model.toJSON())
     );
     return this;
+  },
+  showForm: function(e) {
+    e.preventDefault();
+    $('div#event-join-status').hide();
+    $('div#event-join-form-div').show();
+  },
+  joinEvent: function(e) {
+    e.preventDefault();
+
+    var form = $('form#event-join-form');
+    if (this.validateJoin()) {
+      $('div#event-join-form-div').hide();
+      $('div#event-join-success').show();
+    }
+  },
+  validateJoin: function() {
+    var form = $('form#event-join-form');
+    return true;
   }
 });
