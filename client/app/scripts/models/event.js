623255c4caca93456e713600302ef83d0f5ab072
George Truong <truong.george@gmail.com>
2013-06-29 17:29:45 -0700

    Add filtering by category

--- client/app/scripts/models/event.js
+++ client/app/scripts/models/event.js
@@ -6,24 +6,45 @@ window.Boom.EventCollection = Backbone.Collection.extend({
   comparator: function(event) {
     return event.get('date');
   },
-  sortAndFilter: function(when) {
+  sortAndFilter: function(when, category) {
     var filtered,
         self = this;
     this.data = this.data || this.toJSON();
-    if(when) {
+
+    if (when && category) {
       when = parseInt(when, 10);
       filtered = _.filter(this.data, function(event) {
-        return self.isInTheNextXDays(when, event.date);
+        return self.isInTheNextXDaysWithCategory(when, category, event);
       });
+      this.reset(filtered);
+    } else if(when) {
+      when = parseInt(when, 10);
+      filtered = _.filter(this.data, function(event) {
+        return self.isInTheNextXDays(when, event);
+      });
+      this.reset(filtered);
+    } else if(category) {
+      filtered = _.filter(this.data, function(event) {
+        return self.isMatchingCategory(category, event)
+      })
       this.reset(filtered);
     } else {
       this.reset(this.data);
     }
   },
-  isInTheNextXDays: function(days, date) {
+  isInTheNextXDays: function(days, event) {
     var today = new Date(),
         limit = new Date();
     limit.setDate(today.getDate() + days);
-    return date <= limit;
+    return event.date <= limit;
+  },
+  isInTheNextXDaysWithCategory: function(days, category, event) {
+    var today = new Date(),
+        limit = new Date();
+    limit.setDate(today.getDate() + days);
+    return event.date <= limit && _.indexOf(event.tags, category) !== -1;
+  },
+  isMatchingCategory: function(category, event) {
+    return _.indexOf(event.tags, category) !== -1;
   }
 });
