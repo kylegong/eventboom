93fdfd21dcc813e20b095c305b48c274a0636327
Karl Mendes <karl@darbysmart.com>
2013-06-30 13:22:43 -0700

    Better mock data

--- client/app/scripts/mock_data.js
+++ client/app/scripts/mock_data.js
@@ -3,12 +3,12 @@ window.Boom = window.Boom || {}
 window.Boom.Data = [
   {
     id: 1,
-    title: 'My event',
+    title: 'Basketball time!',
     description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     datetime: new Date(2013, 5, 30, 11),
-    tags: ['sports'],
-    location: 'Lolinda',
-    neighborhood: 'soma',
+    tags: ['Sports'],
+    location: 'Dolores Park',
+    neighborhood: 'SOMA',
     min_attendees: 4,
     max_attendees: 10,
     image: '/images/1.png',
@@ -16,12 +16,12 @@ window.Boom.Data = [
   },
   {
     id: 2,
-    title: 'Other event',
-    description: 'No way!!!',
+    title: 'Dinner out',
+    description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     datetime: new Date(2013, 6, 1, 12, 15),
-    tags: ['food', 'fun'],
+    tags: ['Food', 'Fun'],
     location: 'Sushi place',
-    neighborhood: 'soma',
+    neighborhood: 'SOMA',
     min_attendees: 4,
     max_attendees: 10,
     image: '/images/2.png',
@@ -29,12 +29,12 @@ window.Boom.Data = [
   },
   {
     id: 3,
-    title: 'I\'m not going',
-    description: 'No way!!!',
+    title: 'Prayer night',
+    description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     datetime: new Date(2013, 6, 3, 19, 30),
-    tags: ['speed dating'],
+    tags: ['Prayer'],
     location: 'Ice cream place',
-    neighborhood: 'mission',
+    neighborhood: 'Mission',
     min_attendees: 4,
     max_attendees: 30,
     image: '/images/3.png',
@@ -43,11 +43,11 @@ window.Boom.Data = [
   {
     id: 4,
     title: 'Movie night',
-    description: 'No way!!!',
+    description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     datetime: new Date(2013, 6, 5, 10),
-    tags: ['fun'],
+    tags: ['Fun'],
     location: 'Kyle\'s place',
-    neighborhood: 'mission',
+    neighborhood: 'Mission',
     max_attendees: 10,
     image: '/images/4.png',
     creator_name: 'James'
@@ -55,38 +55,38 @@ window.Boom.Data = [
   {
     id: 5,
     title: 'Taco dinner',
-    description: 'No way!!!',
+    description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     datetime: new Date(2013, 6, 10, 10),
-    tags: ['food'],
+    tags: ['Food'],
     location: 'Kyle\'s place',
-    neighborhood: 'soma',
+    neighborhood: 'SOMA',
     max_attendees: 10,
     image: '/images/5.jpg'
   },
   {
     id: 6,
     title: 'House of Air',
-    description: 'Let\'s go to the house of air!!!',
+    description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     datetime: new Date(2013, 6, 15, 10),
-    tags: ['fun'],
+    tags: ['Fun', 'Sports'],
     location: 'TBD',
-    neighborhood: 'marina',
+    neighborhood: 'Cole Valley',
     max_attendees: 100,
     image: '/images/6.jpg'
   },
   {
     id: 7,
     title: 'Surfin USA',
-    description: 'I want to learn how to surf!',
+    description: "Hi Everyone, <br><br> I’m looking to put together a dinner for 6 at my house in Potrero. Aenean vel tellus mattis, mollis tortor sit amet, interdum tellus. Suspendisse sollicitudin, orci vitae condimentum adipiscing. Maecenas vitae hendrerit arcu, sit amet cursus nulla. Nam faucibus lorem quis risus bibendum, eu placerat arcu bibendum. Sed ornare cursus tellus ac blandit. Praesent malesuada eleifend turpis a laoreet. Proin non leo ultrices lacus sagittis consectetur non non sapien. Nunc ut porttitor ipsum, id auctor augue. Aliquam erat volutpat.",
     datetime: new Date(2013, 6, 4, 10),
-    tags: ['fun', 'sports'],
+    tags: ['Fun', 'Sports'],
     location: 'Pacifica',
-    neighborhood: 'bernal heights',
+    neighborhood: 'Castro',
     max_attendees: 100
   }
 ]
 window.Boom.Data.neighborhoods = ["Alamo Square", "Castro", "Cole Valley", "Cow Hollow", "Haight/Ashbury", "Hayes Valley", "Marina", "Mission", "Noe Valley", "NOPA", "North Beach", "Pac Heights", "Potrero Hill", "Richmond", "SOMA", "Sunset", "Tenderloin", "Western Addition"];
-window.Boom.Data.tags = ["Food", "Fun"];
+window.Boom.Data.tags = ["Food", "Fun", "Sports", "Prayer"];
 window.Boom.Data.get = function(id) {
   return _.find(window.Boom.Data, function(event){
     return event.id === id;
