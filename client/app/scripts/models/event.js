d3717d2ebb2b2cbdb0c19674abea5cf8b3849002
Karl Mendes <karl@darbysmart.com>
2013-06-29 18:37:00 -0700

    More filters

--- client/app/scripts/models/event.js
+++ client/app/scripts/models/event.js
@@ -6,45 +6,33 @@ window.Boom.EventCollection = Backbone.Collection.extend({
   comparator: function(event) {
     return event.get('date');
   },
-  sortAndFilter: function(when, category) {
+  sortAndFilter: function(when, category, neighborhood) {
     var filtered,
         self = this;
     this.data = this.data || this.toJSON();
 
-    if (when && category) {
-      when = parseInt(when, 10);
-      filtered = _.filter(this.data, function(event) {
-        return self.isInTheNextXDaysWithCategory(when, category, event);
-      });
-      this.reset(filtered);
-    } else if(when) {
-      when = parseInt(when, 10);
-      filtered = _.filter(this.data, function(event) {
-        return self.isInTheNextXDays(when, event);
-      });
-      this.reset(filtered);
-    } else if(category) {
-      filtered = _.filter(this.data, function(event) {
-        return self.isMatchingCategory(category, event)
-      })
-      this.reset(filtered);
-    } else {
-      this.reset(this.data);
-    }
+    when = when && parseInt(when, 10);
+
+    filtered = _.filter(this.data, function(event) {
+      var result = true;
+      if (typeof when === 'number') {
+        result = self.isInTheNextXDays(when, event) && result;
+      }
+      if (result && category) {
+        result = _.indexOf(event.tags, category) > -1 && result;
+      }
+      if (result && neighborhood) {
+        result = event.neighborhood == neighborhood && result;
+      }
+      return result;
+    });
+
+    this.reset(filtered);
   },
   isInTheNextXDays: function(days, event) {
     var today = new Date(),
         limit = new Date();
     limit.setDate(today.getDate() + days);
     return event.date <= limit;
-  },
-  isInTheNextXDaysWithCategory: function(days, category, event) {
-    var today = new Date(),
-        limit = new Date();
-    limit.setDate(today.getDate() + days);
-    return event.date <= limit && _.indexOf(event.tags, category) !== -1;
-  },
-  isMatchingCategory: function(category, event) {
-    return _.indexOf(event.tags, category) !== -1;
   }
 });
